00b4f72524c68e213a7e401a33005d5ea7e605f2
Alannazx <alannaescola3d@gmail.com>
2026-03-31 08:34:23 -0300

    Refactor routing logic and add user controller

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 session_start();
 require_once __DIR__ . '/config/db.php';
 Database::getConnection();
+
+
 // Roteamento simples via GET
 $controller = $_GET['controller'] ?? 'auth';
 $action = $_GET['action'] ?? 'form';
@@ -9,34 +11,53 @@ $action = $_GET['action'] ?? 'form';
 
 // Carregar controller
 switch ($controller) {
-case 'auth':
-require_once __DIR__ . '/controllers/AuthController.php';
-$c = new AuthController();
-break;
-//CRUD
-case 'produto':
-require_once __DIR__ . '/controllers/ProdutoController.php';
-$c = new ProdutoController();
-break;
-case 'entrada':
-require_once __DIR__ . '/controllers/EntradaController.php';
-$c = new EntradaController();
-break;
-case 'venda':
-require_once __DIR__ . '/controllers/VendaController.php';
-$c = new VendaController();
-break;
-case 'relatorio':
+    case 'auth':
+        require_once __DIR__ . '/controllers/AuthController.php';
+        $c = new AuthController();
+        break;
 
 
-require_once __DIR__ . '/controllers/RelatorioController.php';
-$c = new RelatorioController();
-break;
-default:
-die("Controller inválido.");
+    // CRUD PRODUTOS E VENDAS
+    case 'produto':
+        require_once __DIR__ . '/controllers/ProdutoController.php';
+        $c = new ProdutoController();
+        break;
+
+
+    case 'entrada':
+        require_once __DIR__ . '/controllers/Entrada_Controller.php';
+        $c = new EntradaController();
+        break;
+
+
+    case 'venda':
+        require_once __DIR__ . '/controllers/VendaController.php';
+        $c = new VendaController();
+        break;
+
+
+    case 'relatorio':
+        require_once __DIR__ . '/controllers/RelatorioController.php';
+        $c = new RelatorioController();
+        break;
+
+
+    // CRUD USUÁRIO / VENDEDOR!!!!!!!!
+    case 'usuario':
+        require_once __DIR__ . '/controllers/UsuarioController.php';
+        $c = new UsuarioController();
+        break;
+
+
+    // Caminho padrão caso o controller não exista
+    default:
+        die("Controller inválido.");
 }
+
 // Executar ação
 if (!method_exists($c, $action)) {
-die("Ação inválida.");
+    die("Ação inválida.");
 }
+
+
 $c->$action();
